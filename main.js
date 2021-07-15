a656f9d59294dcddc715c7658977ad0322e78f0f
Joshua Bartolome <remuwutwut@gmail.com>
2021-07-15 00:24:33 -0700

    all files

---
+++ main.js
@@ -0,0 +1,67 @@
+
+
+//import dotenv package
+
+require('dotenv').config();
+
+//import Discord.js library
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//import coingecko-api
+const CoinGecko = require('coingecko-api');
+var CoingGeckoClient = new CoinGecko();
+
+//Get the current price of Axie SLP vs_currencies PHP
+
+
+
+
+
+function getSLP(data) {
+    return JSON.stringify(data);
+}
+
+//check if Axie Bot is online
+client.on("ready", () => {
+    client.user.setActivity("Axie BiOT", {type: "PLAYING"});
+    //getSLP();
+    console.log("Bot is ready!");
+});
+
+const prefix = "/";
+//Axie message
+client.on("message", msg => {
+    if (msg.content === `${prefix}axie`) {
+        msg.channel.send("```Putang ina mo!```");
+        //msg.channel.send(getSLP());
+        //or if you want to mention name
+        //msg.reply("Putang ina mo!");
+    }
+    else if (msg.content === `${prefix}slp`) {
+        var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+        var xmlhttp = new XMLHttpRequest();
+        var url = "https://api.coingecko.com/api/v3/simple/price?ids=smooth-love-potion&vs_currencies=php";
+
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var data = JSON.parse(this.responseText);
+                var x = JSON.stringify(data['smooth-love-potion'].php);
+                getSLP(data['smooth-love-potion'].php);
+                console.log(x);
+                msg.channel.send("```SLP VALUE:  ₱" + x + "```");
+            }
+            if (msg.content === `${prefix}convert`) {
+
+            }
+    
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
+    
+        
+});
+
+//bot token
+client.login(process.env.BOT_TOKEN);
