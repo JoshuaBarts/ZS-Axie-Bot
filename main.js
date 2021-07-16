07e6ab5d4cd8e66b498a72f1c96fdd342a541d70
JoshuaBarts <80853126+JoshuaBarts@users.noreply.github.com>
2021-07-15 22:27:17 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -14,12 +14,36 @@ var CoingGeckoClient = new CoinGecko();
 
 //Get the current price of Axie SLP vs_currencies PHP
 
+process.setMaxListeners(Infinity);
 
 
 
+function getSLP() {
+    var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://api.coingecko.com/api/v3/simple/price?ids=smooth-love-potion&vs_currencies=php";
+    var num = 0;
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            var x = JSON.stringify(data['smooth-love-potion'].php);
+            console.log("this is x " + isNaN(x) + "= " + x)
+            var num = x;
+            console.log(isNaN(num));
+            
+            
+            
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    xmlhttp.abort();
+    return num;
+    
+}
 
-function getSLP(data) {
-    return JSON.stringify(data);
+function showSLP(data) {
+    return data;
 }
 
 //check if Axie Bot is online
@@ -32,16 +56,24 @@ client.on("ready", () => {
 const prefix = "/";
 //Axie message
 client.on("message", msg => {
+    var content = msg.content.split(" ");
     if (msg.content === `${prefix}axie`) {
         msg.channel.send("Pakyu");
         //msg.channel.send(getSLP());
         //or if you want to mention name
         //msg.reply("Putang ina mo!");
     }
-    else if (msg.content === `${prefix}abu`) {
+    else if (msg.content == `${prefix}abu`) {
         msg.channel.send("rat");
     }
-    else if (msg.content === `${prefix}slp`) {
+    else if (msg.content == `${prefix}hello axie`) {
+        msg.channel.send("You got " + Math.floor(Math.random() * 10) + " free SLP!");
+    }
+    else if (msg.content == `${prefix}axie help`) {
+        msg.channel.send("```COMMANDS \n\n/slp- get slp value \n/convert <number> - convertslp to php \n/hello axie - random slp \n/axie help - list of commands```");
+    }
+    else if (msg.content == `${prefix}slp`) {
+        process.setMaxListeners(Infinity);
         var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
         var xmlhttp = new XMLHttpRequest();
         var url = "https://api.coingecko.com/api/v3/simple/price?ids=smooth-love-potion&vs_currencies=php";
@@ -50,22 +82,44 @@ client.on("message", msg => {
             if (this.readyState == 4 && this.status == 200) {
                 var data = JSON.parse(this.responseText);
                 var x = JSON.stringify(data['smooth-love-potion'].php);
-                getSLP(data['smooth-love-potion'].php);
+                //getSLP();
                 console.log(x);
                 msg.channel.send("SLP VALUE:  ₱" + x + "");
             }
-            if (msg.content === `${prefix}convert`) {
-
-            }
-    
+            
         };
         xmlhttp.open("GET", url, true);
         xmlhttp.send();
+        
+    }
+    else if (content[0] == `${prefix}convert`) {
+        var num = content[1];
+        
+        var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+        var xmlhttp = new XMLHttpRequest();
+        var url = "https://api.coingecko.com/api/v3/simple/price?ids=smooth-love-potion&vs_currencies=php";
+        var xx = 0;
+        var total = 0;
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var data = JSON.parse(this.responseText);
+                var x = JSON.stringify(data['smooth-love-potion'].php);
+                console.log("this is x " + isNaN(x) + "= " + x)
+                xx = x;
+                var number = num * 1;
+                total = number * x;
+                console.log(total);
+                msg.channel.send(number + " SLP x ₱" + x + " = " + "₱" + total.toFixed(2));
+            }
+        }
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+       
+        
     }
     
-    
-        
 });
+
 
 //bot token
 client.login(process.env.BOT_TOKEN);
