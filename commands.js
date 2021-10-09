101ed801c87a9fc0800275889d70b22342aa9609
JoshuaBarts <80853126+JoshuaBarts@users.noreply.github.com>
2021-10-08 21:31:18 -0700

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,26 @@
+const slp = require("./commands/slp.js");
+const SLP = require("./commands/slp.js");
+const axs = require("./commands/axs.js");
+const AXS = require("./commands/axs.js");
+const convertslp = require("./commands/convert-slp.js");
+const convertaxs = require("./commands/convert-axs.js");
+const skill = require("./commands/skill.js");
+const SKILL = require("./commands/skill.js");
+const convertskill = require("./commands/convert-skill.js");
+const axiehelp = require("./commands/axiehelp.js");
+const gif = require("./commands/gif.js");
+
+//DISCORD COMMANDS
+const commands =  {
+    slp, SLP, axs, AXS, convertslp, convertaxs, skill, SKILL, convertskill, axiehelp, gif
+}
+
+module.exports = async function (msg) {
+
+    var tokens = msg.content.split(" ");
+    var command = tokens.shift();
+    if (command.charAt(0) === "/") {
+       command = command.substring(1);
+       commands[command](msg, tokens);
+    }
+}
