fc59c9ce26ca18e2c0fed46cfe262957440a1fe4
Joshua Bartolome <remuwutwut@gmail.com>
2022-01-22 09:19:58 -0800

    fix

--- commands/nuke.js
+++ commands/nuke.js
@@ -3,24 +3,19 @@ const { cheerio } = require("cheerio");
 
 module.exports =  function(msg, args) {
     
-    client.on("ready", async() => {
-        let max = args;
-        if (args.length > 0) {
-            max = args.join(" ");
-        }
-        //parseInt(max);
-        let number = Math.floor(Math.random() * Math.pow(10, 5));
-        
-        //while (number > max || number < 10000) {
-        //    number = Math.floor(Math.random() * Math.pow(10, 5));
-        //}
-        //console.log(number)
-        //console.log(max)
-        let nuke = `https://nhentai.net/g/` + number;
-        msg.delete({timeout: '1000'});
-        let id = msg.author.id;
-        msg.channel.send(nuke)
-    })
+
+    let max = args;
+    if (args.length > 0) {
+        max = args.join(" ");
+    }
+    
+    let number = Math.floor(Math.random() * Math.pow(10, 5));
+    
+    let nuke = `https://nhentai.net/g/` + number;
+    msg.delete({timeout: '1000'});
+    
+    msg.channel.send(nuke)
+    
     
     
 }
