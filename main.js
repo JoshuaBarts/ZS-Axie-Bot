94db8375341f0e9718fd11032268b2a21d17e839
Joshua Bartolome <remuwutwut@gmail.com>
2022-01-21 19:25:44 -0800

    confession bot

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //import dotenv package
-require('dotenv').config();
 
+require('dotenv').config();
+const { MessageEmbed } = require('discord.js');
 //import Discord.js library
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -16,7 +17,7 @@ process.setMaxListeners(Infinity);
 
 //check if Axie Bot is online
 client.on("ready", () => {
-    client.user.setActivity("Axie BiOT", {type: "PLAYING"});
+    client.user.setActivity("Dm me your confessions 😘", {type: "PLAYING"});
     console.log("Bot is ready!");
 });
 
@@ -24,6 +25,38 @@ client.on("ready", () => {
 const commandHandler = require("./commands");
 
 client.on("message", commandHandler);
+
+client.on('message', msg => {
+    if (msg.channel.type == 'dm') {
+        let userContent = msg.content
+        //bot.channels.get('934267055098695703').send(userContent);
+        var guild = client.guilds.cache.get("874519976617324574");
+        var zs = client.guilds.cache.get("565870092219318275"); // get guild by id
+        var channel = zs.channels.cache.get("934286814892654653"); // get channel within guild by id
+        var confessionlog = guild.channels.cache.get("934284586576384000"); //conf log
+        
+        const confessionembed = new MessageEmbed()
+        .setColor("#F1C40F")
+        .setDescription(`"${msg.content}"`)
+        .setAuthor(`Anonymous Confession Arrived | Dm ${client.user.username} to confess 👀`)
+        .setTimestamp();
+
+        const logembed = new MessageEmbed()
+        .setFooter(
+        msg.author.tag + " | " + msg.author.id + " |",
+        msg.author.displayAvatarURL()
+        )
+        .setTitle(`Confession Arrived`)
+
+        .setDescription(`**Confessor :** ${msg.author}\n\n${msg.content}`)
+        .setThumbnail(msg.author.displayAvatarURL({ size: 512, dynamic: true }))
+        .setTimestamp();
+
+        channel.send(confessionembed);
+        confessionlog.send(logembed);
+    }
+});
+
 
 //bot token
 client.login(process.env.BOT_TOKEN);
