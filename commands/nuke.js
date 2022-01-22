ea9552f7082cba434166281ddd4438e772e403ea
Joshua Bartolome <remuwutwut@gmail.com>
2022-01-21 22:48:47 -0800

    nvm

--- commands/nuke.js
+++ commands/nuke.js
@@ -2,23 +2,27 @@ const { default: axios } = require("axios");
 const { cheerio } = require("cheerio");
 const Discord = require('discord.js');
 const client = new Discord.Client();
-module.exports = async function(msg, args) {
+module.exports =  function(msg, args) {
     
-    let max = args;
-    if (args.length > 0) {
-        max = args.join(" ");
-    }
-    //parseInt(max);
-    let number = Math.floor(Math.random() * Math.pow(10, 5));
+    client.on("ready", async() => {
+        let max = args;
+        if (args.length > 0) {
+            max = args.join(" ");
+        }
+        //parseInt(max);
+        let number = Math.floor(Math.random() * Math.pow(10, 5));
+        
+        //while (number > max || number < 10000) {
+        //    number = Math.floor(Math.random() * Math.pow(10, 5));
+        //}
+        //console.log(number)
+        //console.log(max)
+        let nuke = `https://nhentai.net/g/` + number;
+        msg.delete({timeout: '1000'});
+        let id = msg.author.id;
+        const user = await client.users.fetch(id);
+        interaction.user.send(nuke)
+    })
     
-    //while (number > max || number < 10000) {
-    //    number = Math.floor(Math.random() * Math.pow(10, 5));
-    //}
-    //console.log(number)
-    //console.log(max)
-    let nuke = `https://nhentai.net/g/` + number;
-    msg.delete({timeout: '1000'});
-    const user = await client.users.fetch(`${msg.author.id}`);
-    interaction.user.send(nuke)
     
 }
